f4aacd20f44486c819ea7c2284745334f1b46ab2
Eyder Ascuntar <eyder.ascuntar@virginmobilecolombia.com>
2020-03-11 09:32:24 -0500

    Base configurada

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// Require Test routes
-const testRoutes = require('./src/routes/test.route')
 // create express app
 const app = express();
 // Setup server port
@@ -11,6 +9,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
+// Require Test routes
+const testRoutes = require('./src/routes/test.route')
+
 function ignoreFavicon(req, res, next) {
   if (req.originalUrl === '/favicon.ico') {
     res.status(204).json({ nope: true });
@@ -18,21 +19,11 @@ function ignoreFavicon(req, res, next) {
     next();
   }
 }
-
 app.use(ignoreFavicon);
-// define a root/default route
-app.get('/', (req, res) => {
-  console.log('>>>>>>>>>>>>>>> Entrando principal')
-  // try {
-  //   const m = 1;
-  //   const n = m + z;
-  // } catch (err) {
-  //   return res.status(400).send({
-  //     message: 'This is an error!' + err
-  //   });
-  // }
-  res.json({ "message": "Hello World" });
 
+
+app.get('/', (req, res) => {
+  res.json({ "message": "Hello World" });
 });
 // listen for requests
 app.listen(port, () => {
